16168c4a855a850423e7a19c54a894992a01c2f1
Victor Hugo Martins de Jesus <vhmartins.2001@gmail.com>
2023-07-26 22:25:17 -0300

    Initial

---
+++ TestesUnitarios.Console/Models/ValidacoesString.cs
@@ -0,0 +1,23 @@
+
+namespace TestesUnitarios.Console.Models
+{
+    public class ValidacoesString
+    {
+      public int RetornarQuantidadeCaracteres(string texto)
+        {
+            return texto.Length;
+        }
+
+        public bool ContemCaractere(string texto, string textoProcurado)
+        {
+            var contem = texto.Contains(textoProcurado);
+            return contem;
+        }
+
+        public bool TextoTerminaCom(string texto, string textoProcurado)
+        {
+            var termina = texto.EndsWith(textoProcurado);
+            return termina;
+        }
+    }
+}
